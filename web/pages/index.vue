3bc0c88f3ac6df912c68ca7492e078e0cb7807ec
Stephan Schober <teh.fonsi@gmail.com>
2021-12-02 22:13:53 +0100

    feat: added nuxt 3 as frontend

---
+++ web/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>rndr-stats</h1>
+    <h2>coming soon...</h2>
+  </div>
+</template>
---
+++ web/pages/o/[id].vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ result }}
+  </div>
+</template>
+
+<!-- <script setup>
+  // const { data } = await useAsyncData('node-overview', () =>
+  //   $fetch('http://localhost:8888/api/node-overview?id=' + $route.params.id)
+  // );
+  console.log(this);
+</script> -->
+
+<script>
+  export default {
+    data: () => ({ result: null }),
+    async asyncData() {},
+    async mounted() {
+      const { id } = this.$route.params;
+      // // const { data } = await useAsyncData('node-overview', () =>
+      // //   $fetch('http://localhost:8888/api/node-overview?id=' + id)
+      // // );
+      // const url = '/api/node-overview?id=' + id;
+      // console.log(url);
+      // const { data } = await $fetch(url);
+      // console.log(data.value);
+      this.result = id;
+    },
+  };
+</script>
