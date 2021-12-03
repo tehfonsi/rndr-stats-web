435717ee42e0fcab9da745b4c462a32f8a07d4a5
Stephan Schober <teh.fonsi@gmail.com>
2021-12-03 14:47:41 +0100

    feat: marry netlify and nuxt

--- web/pages/o/[id].vue
+++ web/pages/o/[id].vue
@@ -1,30 +1,17 @@
 <template>
   <div>
-    {{ result }}
+    {{ overview }}
   </div>
 </template>
 
-<!-- <script setup>
-  // const { data } = await useAsyncData('node-overview', () =>
-  //   $fetch('http://localhost:8888/api/node-overview?id=' + $route.params.id)
-  // );
-  console.log(this);
-</script> -->
-
 <script>
   export default {
-    data: () => ({ result: null }),
+    data: () => ({ overview: null }),
     async asyncData() {},
     async mounted() {
       const { id } = this.$route.params;
-      // // const { data } = await useAsyncData('node-overview', () =>
-      // //   $fetch('http://localhost:8888/api/node-overview?id=' + id)
-      // // );
-      // const url = '/api/node-overview?id=' + id;
-      // console.log(url);
-      // const { data } = await $fetch(url);
-      // console.log(data.value);
-      this.result = id;
+      const url = '/api/node-overview?id=' + id;
+      this.overview = await $fetch(url);
     },
   };
 </script>
