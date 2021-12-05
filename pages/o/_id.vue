aa6e58cdeea1b786987e750abb1cb0688a131bf3
Stephan Schober <teh.fonsi@gmail.com>
2021-12-05 22:27:31 +0100

    feat: use nuxt 2 for frontend

---
+++ pages/o/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ nodes }}</div>
+</template>
+
+<script>
+  export default {
+    async asyncData({ params }) {
+      const { id } = params;
+      const nodes = await fetch(
+        'https://rndr-stats.netlify.app/api/node-overview?id=' + id
+      ).then((res) => res.json());
+      return { nodes };
+    },
+  };
+</script>
