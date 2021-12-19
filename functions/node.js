ac6b33611e9469170479862d900fbe03aa79ba10
Stephan Schober <teh.fonsi@gmail.com>
2021-12-19 20:00:12 +0100

    fix: set parameters to 0 if not set

--- functions/node.js
+++ functions/node.js
@@ -5,12 +5,17 @@ const newNode = async (event) => {
   const {eth_address, node_id, score, previews_sent, jobs_completet, thumbnails_sent, gpus, password} = JSON.parse(event.body);
   const operator_id = hash(eth_address);
 
+  score = !!score ? parseInt(score) : 0;
+  previews_sent = !!previews_sent ? parseInt(previews_sent) : 0;
+  jobs_completet = !!jobs_completet ? parseInt(jobs_completet) : 0;
+  thumbnails_sent = !!thumbnails_sent ? parseInt(thumbnails_sent) : 0;
+
   const node = {
     id: node_id,
-    score: parseInt(score),
-    previews_sent: parseInt(previews_sent),
-    jobs_completed: parseInt(jobs_completet),
-    thumbnails_sent: parseInt(thumbnails_sent),
+    score,
+    previews_sent,
+    jobs_completed: jobs_completet,
+    thumbnails_sent,
     gpus,
     operator: operator_id,
     password
